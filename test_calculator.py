2df73da68baa47a24b2f85990f62d7e43e9e37a7
bodlz <bodeloison8@gmail.com>
2024-04-12 15:27:18 +0200

    test

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,17 @@ from calculator import addition
 
 def test_addition():
     assert addition(1, 2) == 3
+
+from calculator import soustraction
+
+
+
+def test_substraction():
+    assert soustraction(5, 2) == 3
+
+from calculator import division
+
+
+
+def test_division():
+    assert division(10, 2) == 5
